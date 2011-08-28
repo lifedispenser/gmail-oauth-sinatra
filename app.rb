674d24174276426bbe4585e617316de76bd0da3e
Spencer Ying <lifedispenser@gmail.com>
2011-08-28 16:31:12 -0400

    first commit

--- app.rb
+++ app.rb
@@ -4,14 +4,16 @@ require 'json'
 require "oauth"
 require "oauth/consumer"
 require 'haml'
+require 'gmail'
 
 enable :sessions
 
+#this sets up the oauth configurations, like the request path and the token path
 before do
   session[:oauth] ||= {}  
   
-  consumer_key = ENV["CONSUMER_KEY"] || ENV["consumer_key"]
-  consumer_secret = ENV["CONSUMER_SECRET"] || ENV["consumer_secret"]
+  consumer_key = "anonymous"
+  consumer_secret = "anonymous"
   
   @consumer ||= OAuth::Consumer.new(consumer_key, consumer_secret,
     :site => "https://www.google.com",
@@ -31,28 +33,38 @@ before do
 end
 
 get "/" do
-  if @access_token
-    response = @access_token.get('https://www.googleapis.com/userinfo/email?alt=json')
-    if response.is_a?(Net::HTTPSuccess)
-      @email = JSON.parse(response.body)['data']['email']
-    else
-      STDERR.puts "could not get email: #{response.inspect}"
-    end
+  if @access_token #if we have received an access token on our request (meaning we have authenticated)
+    gmail = Gmail.connect(:xoauth, "_a throw away user email here_", #use your personal one at your own risk
+			:token           => @access_token.token,
+			:secret          => @access_token.secret,
+			:consumer_key    => 'anonymous',
+			:consumer_secret => 'anonymous'
+		)
+		# As long as you have @access_token.token and @access_token.secret, you can authenticate into gmail
+		
+		
+		# After authentication, you can test out the functions you want to retrieve emails:
+		# look in mailbox.rb of def emails in the gmail gem to see the options available
+		
+		@inbox = gmail.inbox.emails # > an array of sequence numbers [1, 3, 5, 6, ..., #]
+		# gmail.inbox.emails(:search => "custom email").collect { |x| x.uid }
+		# gmail.inbox.emails(:to => "Me")
+		
     haml :index
-  else
+  else #otherwise, sign in at /request
     '<a href="/request">Sign On</a>'
   end
 end
 
-get "/request" do
-  @request_token = @consumer.get_request_token(:oauth_callback => "#{request.scheme}://#{request.host}:#{request.port}/auth")
+get "/request" do #send us to gmail to get an auth_request token and secret
+  @request_token = @consumer.get_request_token(:oauth_callback => "#{request.scheme}://#{request.host}:#{request.port}/auth") #redirect_url is at /auth
   session[:oauth][:request_token] = @request_token.token
   session[:oauth][:request_token_secret] = @request_token.secret
   redirect @request_token.authorize_url
 end
 
-get "/auth" do
-  @access_token = @request_token.get_access_token :oauth_verifier => params[:oauth_verifier]
+get "/auth" do #once we've been redirected here, receive the access token and secret and use it to authenticate to gmail: 
+  @access_token = @request_token.get_access_token :oauth_verifier => params[:oauth_verifier] 
   session[:oauth][:access_token] = @access_token.token
   session[:oauth][:access_token_secret] = @access_token.secret
   redirect "/"
